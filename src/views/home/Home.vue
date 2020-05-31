3849ed067e6caf51d846bc0e7bec648e430ff7ab
OliverMessi <562376662@qq.com>
2020-05-31 22:27:50 +0800

    '首页轮播图展示'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,34 @@
 <template>
  <div id="home">
    <nav-bar class="home-nav"><div slot="center">豆瓣图书</div></nav-bar>
+   <home-swiper :subjects="subjects"/>
  </div>
 </template>
 
 <script>
 
   import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper"
+
+  import {getHomeMultidata} from "network/home";
+
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
+    },
+    data(){
+      return{
+        subjects:[],//存放热映的电影数组数据
+      }
+    },
+    created(){
+      //1.请求多个数据
+      getHomeMultidata().then(res=>{
+        console.log(res);
+         this.subjects = res.subjects;
+      })
     }
   }
 </script>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in subjects">
+      <a :href="item.alt">
+        <img :src="item.images.small" alt="未显示" height="200"/>
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper,SwiperItem} from 'components/common/swiper';
+
+  export default {
+    name: "HomeSwiper",
+    props:{
+      subjects:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    components:{
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
